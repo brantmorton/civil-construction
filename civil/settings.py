aab627fcedac88f8704f1ca6851f9d3153ec0bdd
root <root@LAPTOP-JEOQHU6P.localdomain>
2019-06-25 16:22:30 -0500

    complete front-end overhaul

--- civil/settings.py
+++ civil/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('CIVIL_PROJECT_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'e6ac8ae2.ngrok.io',
+    'localhost'
+]
 
 
 # Application definition
---
+++ marketing/static/marketing/mobile.js
@@ -0,0 +1,7 @@
+function OpenNav() {
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function CloseNav() {
+    document.getElementById("mobile-menu").style.width = "0";
+}
